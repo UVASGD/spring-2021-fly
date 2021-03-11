6582e7c7f405b383c0bf7d0f707f04a1cd1da87e
ryanakann <ryanakann@gmail.com>
2021-03-10 20:41:56 -0500

    EXPERIENCE TRANQUILITY

--- Fly/Assets/Scripts/RunManager.cs
+++ Fly/Assets/Scripts/RunManager.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class RunManager : MonoBehaviour
 {
+    private bool restartable;
 
+    [Header("References")]
     public GameObject canvas;
 
-    private bool restartable;
+    [Header("Goal Info")]
+    public Vector3 goalPosition;
+    public float goalRadius = 5f;
+    public float goalHeight = 5f;
+
+    [Header("Run Stats")]
+    public float distanceTravelled;
+    public float distanceFromGoal;
+    public float maxHeightThisRun;
+    public float currentHeight;
+
 
     private void Start()
     {
@@ -25,6 +37,9 @@ public class RunManager : MonoBehaviour
 
     private void Update()
     {
+        currentHeight = GameManager.instance.playerController.transform.position.y;
+        maxHeightThisRun = Mathf.Max(maxHeightThisRun, currentHeight);
+
         if (!restartable) return;
 
         if (Input.GetKeyDown(KeyCode.R))
