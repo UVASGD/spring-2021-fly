d2aac313e48cf1ae5c79e06349289be09d0cf8d1
Jimmy Connors <jjc9bb@virginia.edu>
2021-03-19 18:41:45 -0400

    Put object on terrain, test scene/mesa biome
    added a script to make objects snap to the terrain when the scene starts (essentially makes it so that their y value= where the terrain's y will be).
    Also tested some things with the terrain generation, adding a new testing scene and testing terrain assets. They make a simple mesa biome, looks pretty good all things considered.

---
+++ Fly/Assets/Scripts/PutObjectOnTerrain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutObjectOnTerrain : MonoBehaviour
+{
+    public Transform tf;
+    public MeshSettings meshSettings;
+    public HeightMapSettings heightMapSettings;
+    // Start is called before the first frame update
+
+    //currently have it updating every frame for testing reasons- will change to only on start when actually doing things
+
+    void Start()
+    {
+        float meshWorldSize = meshSettings.meshWorldSize;
+        float xPos = tf.position.x;
+        float zPos = tf.position.z;
+        Vector2 thisLocation = new Vector2(xPos, zPos);
+        float[,] hm = Noise.GenerateNoiseMap(1,1,heightMapSettings.noiseSettings,thisLocation);
+        float height = hm[0, 0];
+        AnimationCurve heightCurve_threadsafe = new AnimationCurve(heightMapSettings.heightCurve.keys);
+        height *= heightCurve_threadsafe.Evaluate(height) * heightMapSettings.heightMultiplier;
+        transform.position = new Vector3(xPos, height, zPos);
+    }
+
+    // Update is called once per frame
+
+}
+
