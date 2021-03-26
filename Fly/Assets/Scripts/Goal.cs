a95d54c7145cb9da769bd5d78d1477ffb058905e
ryanakann <ryanakann@gmail.com>
2021-03-26 18:31:21 -0400

    Goal state and updates to gamemanager prefab

---
+++ Fly/Assets/Scripts/Goal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+    public RectTransform uiTransform;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        uiTransform.anchoredPosition = screenPos;
+        //uiTransform.position = transform.position;
+    }
+}
