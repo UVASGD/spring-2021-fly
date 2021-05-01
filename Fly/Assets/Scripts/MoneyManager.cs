a77b8a3362ec12aba2ac5ef5d6370379eb4cb0ce
ryanakann <ryanakann@gmail.com>
2021-04-30 21:40:43 -0400

    Shopping spree

---
+++ Fly/Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour, ISavable
+{
+    public static MoneyManager instance;
+
+    public float startingMoney;
+    private float money;
+
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey("Money"))
+        {
+            money = PlayerPrefs.GetFloat("Money");
+        }
+        else
+        {
+            money = startingMoney;
+            Save();
+        }
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("Money", money);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Fly/Assets/Scripts/ShopButton.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopButton : MonoBehaviour, ISavable
+{
+    public ShopButton prereq;
+    public GameObject lockedOverlay;
+    public GameObject purchasedOverlay;
+
+    public bool locked;
+    public bool purchased;
+
+    public bool autoUnlock;
+
+    public void Load()
+    {
+        string lockedKey = $"{gameObject.name}-locked";
+        string purchasedKey = $"{gameObject.name}-purchased";
+
+        if (PlayerPrefs.HasKey(lockedKey))
+        {
+            locked = PlayerPrefs.GetInt(lockedKey) == 0 ? false : true;
+        }
+        else
+        {
+            locked = !autoUnlock;
+        }
+
+        if (PlayerPrefs.HasKey(purchasedKey))
+        {
+            purchased = PlayerPrefs.GetInt(purchasedKey) == 0 ? false : true;
+        }
+        
+        UpdateButton();
+    }
+
+    public void Save()
+    {
+        string lockedKey = $"{gameObject.name}-locked";
+        string purchasedKey = $"{gameObject.name}-purchased";
+        PlayerPrefs.SetInt(lockedKey, locked ? 1 : 0);
+        PlayerPrefs.SetInt(purchasedKey, purchased ? 1 : 0);
+    }
+
+    public void UpdateButton()
+    {
+        if (purchased)
+        {
+            purchasedOverlay.SetActive(true);
+            lockedOverlay.SetActive(false);
+        }
+        else if (locked)
+        {
+            purchasedOverlay.SetActive(false);
+            lockedOverlay.SetActive(true);
+        }
+        else
+        {
+            purchasedOverlay.SetActive(false);
+            lockedOverlay.SetActive(false);
+        }
+    }
+
+    public void TryPurchase()
+    {
+
+    }
+}
